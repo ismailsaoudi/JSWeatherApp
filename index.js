189d9faf319e35a81e8ae5094c19039e538708b1
ismail saoudi <ismail.saoudi@icloud.com>
2022-10-10 13:29:08 -0400

    html DOM

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const key = "32fbbd3e4014481fe555bbcc5505abcc";
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${key}`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=<<cityName>>&units=metric&apikey=${key}`;
 
 
 
