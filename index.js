4003d57b614eb4242b40f4d9d5f63f27226a0c81
ismail saoudi <ismail.saoudi@icloud.com>
2022-10-10 16:02:18 -0400

    js function tested

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+
+
 const key = "32fbbd3e4014481fe555bbcc5505abcc";
+ const cityName = "Haffouz"
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=<<cityName>>&units=metric&apikey=${key}`;
+ const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&apikey=${key}`;
 
 
 
-function fetchWeather(query){
-    fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+ function fetchWeather(data){
+     fetch(apiUrl)
+     .then((response) => response.json())
+     .then((data) => console.log(data));
+ }
+
+ fetchWeather()
+
+  
+const city = document.getElementById(searchField)
+const searchBtn = document.getElementById(searchBtn)
+
+function displayData (){
+    if (city === cityName){
+        return 
+    }
 }
-
-fetchWeather();
-
