f1734b56db57df67911f6bd105cc1d91823320b7
ismail saoudi <ismail.saoudi@icloud.com>
2022-10-10 10:17:30 -0400

    tested the Api key

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const apiUrl = ""
+const key = "32fbbd3e4014481fe555bbcc5505abcc";
+
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${key}`;
+
+
+
+function fetchWeather(query){
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+fetchWeather();
+
