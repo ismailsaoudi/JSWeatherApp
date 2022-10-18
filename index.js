c4976990bab5393c8c52117ccb942a77fe1fccf8
ismail saoudi <ismail.saoudi@icloud.com>
2022-10-18 09:30:04 -0400

    current Weather function test

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
+document.getElementById("searchBtn").addEventListener("click", (event) => {
+  var cityName = document.getElementById("cityName").value;
+  var key = "32fbbd3e4014481fe555bbcc5505abcc";
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`;
 
-document.getElementById('searchBtn').addEventListener('click', event => {
-    let cityName = document.getElementById('cityName').value
-    let key = "32fbbd3e4014481fe555bbcc5505abcc"
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&apikey=${key}`
-   
-    fetch(apiUrl)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  console.log(cityName);
+  fetch(apiUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data);
 
-    console.log(cityName)
+      var cardTitle = data.name;
+      var temp = data.main.temp;
+      var wind = data.wind.speed;
+      var currentDate = new Date();
+      var cDay = currentDate.getDate();
+      var cMonth = currentDate.getMonth() + 1;
+      var cYear = currentDate.getFullYear();
+      var time = currentDate.getHours() + ":" + currentDate.getMinutes();
 
-//     let lat = res.data.city.coord.lat
-//     let lon = res.data.city.coord.lon
+      // var lat = data.coord.lat;
+      // var lon = data.city.coord.lon;
 
-//     let uviUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${key}`
-
-// fetch(uviUrl)
-// .then((resp) => resp.json())
-// .then(() => console.log(resp.data));
-
-})
-
- 
-
-
- 
-
-
-
-
+      document.getElementById("currentWeather").innerHTML = `
+    <div class="row">
+      <div class="col s12 m6">
+       <div class="card blue-grey darken-1">
+         <div class="card-content white-text">
+          <span class="card-title">${cardTitle}</span>
+          <span class="card-title">${cDay}/${cMonth}/${cYear}  ${time}</span>
+          
+        <ul class="result"  >
+          <li class="temp" >Temperature ${temp} C</li>
+          <li class="wind">Wind ${wind} KM/H</li>
+          <li class="humidity"></li>
+        </ul>
+      </div>
+    </div>
+    </div> `;
+    });
+});
