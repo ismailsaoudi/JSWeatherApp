e7dbcf49a13b6f51a0eba3f7fa571aaa7f4d9c65
ismail saoudi <ismail.saoudi@icloud.com>
2022-10-18 12:15:37 -0400

    5 days forecast tested

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-document.getElementById("searchBtn").addEventListener("click", (event) => {
-  var cityName = document.getElementById("cityName").value;
-  var key = "32fbbd3e4014481fe555bbcc5505abcc";
-  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`;
+ document.getElementById("searchBtn").addEventListener("click", (event) => {
+   var cityName = document.getElementById("cityName").value;
+   var key = "32fbbd3e4014481fe555bbcc5505abcc";
+   var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`;
 
-  console.log(cityName);
+  
   fetch(apiUrl)
     .then(function (res) {
       return res.json();
@@ -14,30 +14,133 @@ document.getElementById("searchBtn").addEventListener("click", (event) => {
       var cardTitle = data.name;
       var temp = data.main.temp;
       var wind = data.wind.speed;
+      var humidity = data.main.humidity;
       var currentDate = new Date();
       var cDay = currentDate.getDate();
-      var cMonth = currentDate.getMonth() + 1;
+      var cMonth = currentDate.getMonth()+1;
       var cYear = currentDate.getFullYear();
-      var time = currentDate.getHours() + ":" + currentDate.getMinutes();
+      var cdescription = data.weather[0].description;
+      
 
       // var lat = data.coord.lat;
-      // var lon = data.city.coord.lon;
+       // var lon = data.city.coord.lon;
 
       document.getElementById("currentWeather").innerHTML = `
-    <div class="row">
-      <div class="col s12 m6">
-       <div class="card blue-grey darken-1">
-         <div class="card-content white-text">
-          <span class="card-title">${cardTitle}</span>
-          <span class="card-title">${cDay}/${cMonth}/${cYear}  ${time}</span>
+     <div class="row">
+       <div class="col s10 m5">
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+           <span class="card-title">${cardTitle}</span>
+           <span class="card-title">${cDay}/${cMonth}/${cYear}</span>
+           <span class="card-title"> Described as:  ${cdescription}</span>
+         
+        
           
         <ul class="result"  >
-          <li class="temp" >Temperature ${temp} C</li>
-          <li class="wind">Wind ${wind} KM/H</li>
-          <li class="humidity"></li>
+          <li class="temp" ><img src="./images/temperature.png" alt="Temperature" width="50" height="60"> ${temp} °C </li>
+          <li class="wind"><img src="./images/wind.png" alt="wind" width="50" height="60"> ${wind} KM/H</li>
+          <li class="humidity"> <img src="./images/humidity.png" alt="humidity" width="50" height="60"> ${humidity}  G.m-3</li>
         </ul>
+      </div>     
       </div>
-    </div>
     </div> `;
     });
 });
+
+
+document.getElementById("searchBtn").addEventListener("click", (event) => {
+    var cityName = document.getElementById("cityName").value;
+    var key = "32fbbd3e4014481fe555bbcc5505abcc";
+    var apiUrl5Days = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}&units=metric`;
+  
+   
+    fetch(apiUrl5Days)
+      .then(function (resp) {
+        return resp.json();
+      })
+      .then(function (data1) {
+        console.log(data1);
+
+        var temp1 = data1.list[1].main.temp;
+        var wind1 = data1.list[1].wind.speed;
+        var humidity1 = data1.list[1].main.humidity;
+        var date1 =data1.list[1].dt
+           
+
+        var temp2 = data1.list[2].main.temp;
+        var wind2 = data1.list[2].wind.speed;
+        var humidity2 = data1.list[2].main.humidity;
+        var date2 =data1.list[2].dt
+
+        var temp3 = data1.list[3].main.temp;
+        var wind3 = data1.list[3].wind.speed;
+        var humidity3 = data1.list[3].main.humidity;
+        var date3 =data1.list[3].dt
+
+        var temp4 = data1.list[4].main.temp;
+        var wind4 = data1.list[4].wind.speed;
+        var humidity4 = data1.list[4].main.humidity;
+        var date4 =data1.list[4].dt
+    
+        document.getElementById("5daysWeather").innerHTML = `
+      <div class="row">
+        <div class="col s4 m2">
+          <div class="card blue-grey darken-1">
+           <div class="card-content white-text">
+            <span class="card-title"> Date: ${date1}</span>
+            <ul class="result"  >
+          <li class="temp" ><img src="./images/temperature.png" alt="Temperature" width="50" height="60"> ${temp1} °C </li>
+          <li class="wind"><img src="./images/wind.png" alt="wind" width="50" height="60"> ${wind1} KM/H</li>
+          <li class="humidity"> <img src="./images/humidity.png" alt="humidity" width="50" height="60"> ${humidity1}  G.m-3</li>
+        </ul>
+           </div>
+          </div>
+        </div>
+      </div> 
+      <div class="row">
+      <div class="col s4 m2">
+        <div class="card blue-grey darken-1">
+         <div class="card-content white-text">
+          <span class="card-title"> Date: ${date2}</span>
+          <ul class="result"  >
+        <li class="temp" ><img src="./images/temperature.png" alt="Temperature" width="50" height="60"> ${temp2} °C </li>
+        <li class="wind"><img src="./images/wind.png" alt="wind" width="50" height="60"> ${wind2} KM/H</li>
+        <li class="humidity"> <img src="./images/humidity.png" alt="humidity" width="50" height="60"> ${humidity2}  G.m-3</li>
+      </ul>
+         </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s4 m2">
+      <div class="card blue-grey darken-1">
+       <div class="card-content white-text">
+        <span class="card-title"> Date: ${date3}</span>
+        <ul class="result"  >
+      <li class="temp" ><img src="./images/temperature.png" alt="Temperature" width="50" height="60"> ${temp3} °C </li>
+      <li class="wind"><img src="./images/wind.png" alt="wind" width="50" height="60"> ${wind3} KM/H</li>
+      <li class="humidity"> <img src="./images/humidity.png" alt="humidity" width="50" height="60"> ${humidity3}  G.m-3</li>
+    </ul>
+       </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+  <div class="col s4 m2">
+    <div class="card blue-grey darken-1">
+     <div class="card-content white-text">
+      <span class="card-title"> Date: ${date4}</span>
+      <ul class="result"  >
+    <li class="temp" ><img src="./images/temperature.png" alt="Temperature" width="50" height="60"> ${temp4} °C </li>
+    <li class="wind"><img src="./images/wind.png" alt="wind" width="50" height="60"> ${wind4} KM/H</li>
+    <li class="humidity"> <img src="./images/humidity.png" alt="humidity" width="50" height="60"> ${humidity4}  G.m-3</li>
+  </ul>
+     </div>
+    </div>
+  </div>
+</div>
+    `; 
+   
+
+      });
+  });
